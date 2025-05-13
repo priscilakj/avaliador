2b6591fed40b68c79262120666a7cc990242fd8c
priscilakj <kaji.priscila@gmail.com>
2025-05-13 20:18:22 -0300

    Update firebaseConfig.ts

--- src/config/firebaseConfig.ts
+++ src/config/firebaseConfig.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-   apiKey: "AIzaSyDIw3ShG244eC395zVMq5IvWKo2it7OW8M",
-  authDomain: "crud-questions.firebaseapp.com",
-  projectId: "crud-questions",
-  storageBucket: "crud-questions.firebasestorage.app",
-  messagingSenderId: "684024395348",
-  appId: "1:684024395348:web:83e5023015cd66295b1fc1",
-  measurementId: "G-20K08WRQ35"
+  apiKey: "AIzaSyDCByacqfbC9-ISdQIJJD7D0hTSCiD0LVU",
+  authDomain: "avaliador2-39ec4.firebaseapp.com",
+  projectId: "avaliador2-39ec4",
+  storageBucket: "avaliador2-39ec4.firebasestorage.app",
+  messagingSenderId: "956383625001",
+  appId: "1:956383625001:web:17dd8604b9c2b307cb0138",
+  measurementId: "G-JVKX0032LT"
 };
 
 const app = initializeApp(firebaseConfig);
