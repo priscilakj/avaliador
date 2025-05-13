3f82d53654e2801e8668f70333896995bcf413f2
priscilakj <kaji.priscila@gmail.com>
2025-05-13 18:50:48 -0300

    Update firebaseConfig.ts

--- src/config/firebaseConfig.ts
+++ src/config/firebaseConfig.ts
@@ -3,12 +3,12 @@ import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
     apiKey: "AIzaSyCurk1O9u59u941JsF9HlzABXq0C4RA9Ko",
-    authDomain: "avaliador-83255.firebaseapp.com",
-    projectId: "avaliador-83255",
-    storageBucket: "avaliador-83255.firebasestorage.app",
-    messagingSenderId: "976828553565",
-    appId: "1:976828553565:web:7241a28f7148f5435df0b4",
-    measurementId: "G-MESTR7FX58"
+  authDomain: "avaliador-83255.firebaseapp.com",
+  projectId: "avaliador-83255",
+  storageBucket: "avaliador-83255.firebasestorage.app",
+  messagingSenderId: "976828553565",
+  appId: "1:976828553565:web:7241a28f7148f5435df0b4",
+  measurementId: "G-MESTR7FX58"
 };
 
 const app = initializeApp(firebaseConfig);
